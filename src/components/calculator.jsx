71f05ef9f6957757c6007b1ca4cf55000ed4c314
diego <dgcuenca@gmail.com>
2023-01-10 16:35:44 -0500

    Layout calculator interface

--- src/components/calculator.jsx
+++ src/components/calculator.jsx
@@ -1,8 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Calculator extends Component {
   render() {
-    return <h1>Hello World</h1>;
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col bg-secondary text-end border-secondary text-white">0</div>
+        </div>
+				<div className="row">
+          <div className="col-3 bg-light text-center border-secondary border border-1">AC</div>
+					<div className="col-3 bg-light text-center border-secondary border border-1">+/-</div>
+					<div className="col-3 bg-light text-center border-secondary border border-1">%</div>
+					<div className="col-3 bg-warning text-center border-secondary border border-1">/</div>
+        </div>
+				<div className="row">
+          <div className="col-3 bg-light text-center border-secondary border border-1">7</div>
+					<div className="col-3 bg-light text-center border-secondary border border-1">8</div>
+					<div className="col-3 bg-light text-center border-secondary border border-1">9</div>
+					<div className="col-3 bg-warning text-center border-secondary border border-1">*</div>
+        </div>
+				<div className="row">
+          <div className="col-3 bg-light text-center border-secondary border border-1">4</div>
+					<div className="col-3 bg-light text-center border-secondary border border-1">5</div>
+					<div className="col-3 bg-light text-center border-secondary border border-1">6</div>
+					<div className="col-3 bg-warning text-center border-secondary border border-1">-</div>
+        </div>
+				<div className="row">
+          <div className="col-3 bg-light text-center border-secondary border border-1">1</div>
+					<div className="col-3 bg-light text-center border-secondary border border-1">2</div>
+					<div className="col-3 bg-light text-center border-secondary border border-1">3</div>
+					<div className="col-3 bg-warning text-center border-secondary border border-1">+</div>
+        </div>
+				<div className="row">
+          <div className="col-6 bg-light text-center border-secondary border border-1">0</div>
+					<div className="col-3 bg-light text-center border-secondary border border-1">.</div>
+					<div className="col-3 bg-warning text-center border-secondary border border-1">=</div>
+        </div>
+      </div>
+    );
   }
 }
 
