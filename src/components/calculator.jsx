fbdebdb0d2ea64c5759c171f9353e3568b71469e
diego <dgcuenca@gmail.com>
2023-01-11 16:30:42 -0500

    Add events to buttons calculator

--- src/components/calculator.jsx
+++ src/components/calculator.jsx
@@ -9,6 +9,10 @@ class Calculator extends Component {
     this.state = {};
   }
 
+  onChangeHandler = (event) => {
+    console.log(event.target.innerText, typeof event.target.innerText);
+  };
+
   render() {
     return (
       <div className="container w-75">
@@ -16,33 +20,33 @@ class Calculator extends Component {
           <div className="col bg-secondary text-end border-secondary text-white">0</div>
         </div>
 				<div className="row">
-          <div className="col-3 bg-light text-center border-secondary border border-1">AC</div>
-					<div className="col-3 bg-light text-center border-secondary border border-1">+/-</div>
-					<div className="col-3 bg-light text-center border-secondary border border-1">%</div>
-					<div className="col-3 bg-warning text-center border-secondary border border-1">/</div>
+          <button className="col-3 bg-light text-center border-secondary border border-1" onClick={this.onChangeHandler}>AC</button>
+					<button className="col-3 bg-light text-center border-secondary border border-1" onClick={this.onChangeHandler}>+/-</button>
+					<button className="col-3 bg-light text-center border-secondary border border-1" onClick={this.onChangeHandler}>%</button>
+					<button className="col-3 bg-warning text-center border-secondary border border-1" onClick={this.onChangeHandler}>/</button>
         </div>
 				<div className="row">
-          <div className="col-3 bg-light text-center border-secondary border border-1">7</div>
-					<div className="col-3 bg-light text-center border-secondary border border-1">8</div>
-					<div className="col-3 bg-light text-center border-secondary border border-1">9</div>
-					<div className="col-3 bg-warning text-center border-secondary border border-1">*</div>
+          <button className="col-3 bg-light text-center border-secondary border border-1" onClick={this.onChangeHandler}>7</button>
+					<button className="col-3 bg-light text-center border-secondary border border-1" onClick={this.onChangeHandler}>8</button>
+					<button className="col-3 bg-light text-center border-secondary border border-1" onClick={this.onChangeHandler}>9</button>
+					<button className="col-3 bg-warning text-center border-secondary border border-1" onClick={this.onChangeHandler}>*</button>
         </div>
 				<div className="row">
-          <div className="col-3 bg-light text-center border-secondary border border-1">4</div>
-					<div className="col-3 bg-light text-center border-secondary border border-1">5</div>
-					<div className="col-3 bg-light text-center border-secondary border border-1">6</div>
-					<div className="col-3 bg-warning text-center border-secondary border border-1">-</div>
+          <button className="col-3 bg-light text-center border-secondary border border-1" onClick={this.onChangeHandler}>4</button>
+					<button className="col-3 bg-light text-center border-secondary border border-1" onClick={this.onChangeHandler}>5</button>
+					<button className="col-3 bg-light text-center border-secondary border border-1" onClick={this.onChangeHandler}>6</button>
+					<button className="col-3 bg-warning text-center border-secondary border border-1" onClick={this.onChangeHandler}>-</button>
         </div>
 				<div className="row">
-          <div className="col-3 bg-light text-center border-secondary border border-1">1</div>
-					<div className="col-3 bg-light text-center border-secondary border border-1">2</div>
-					<div className="col-3 bg-light text-center border-secondary border border-1">3</div>
-					<div className="col-3 bg-warning text-center border-secondary border border-1">+</div>
+          <button className="col-3 bg-light text-center border-secondary border border-1" onClick={this.onChangeHandler}>1</button>
+					<button className="col-3 bg-light text-center border-secondary border border-1" onClick={this.onChangeHandler}>2</button>
+					<button className="col-3 bg-light text-center border-secondary border border-1" onClick={this.onChangeHandler}>3</button>
+					<button className="col-3 bg-warning text-center border-secondary border border-1" onClick={this.onChangeHandler}>+</button>
         </div>
 				<div className="row">
-          <div className="col-6 bg-light text-center border-secondary border border-1">0</div>
-					<div className="col-3 bg-light text-center border-secondary border border-1">.</div>
-					<div className="col-3 bg-warning text-center border-secondary border border-1">=</div>
+          <button className="col-6 bg-light text-center border-secondary border border-1" onClick={this.onChangeHandler}>0</button>
+					<button className="col-3 bg-light text-center border-secondary border border-1" onClick={this.onChangeHandler}>.</button>
+					<button className="col-3 bg-warning text-center border-secondary border border-1" onClick={this.onChangeHandler}>=</button>
         </div>
       </div>
     );
