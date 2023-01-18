8b5cf8b7bdc42b3f648d1e3d7524cc20409f1280
diego <dgcuenca@gmail.com>
2023-01-18 11:59:26 -0500

    Complete calculator page

--- src/components/calculator.jsx
+++ src/components/calculator.jsx
@@ -1,59 +1,62 @@
 import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 
-const Calculator = ({total: initialTotal, next: initialNext, operation: initialOperation }) => {
-	
+const Calculator = ({ total: initialTotal, next: initialNext, operation: initialOperation }) => {
   const [total, setTotal] = useState(initialTotal);
   const [next, setNext] = useState(initialNext);
-  const [operation, setOperation] = useState(initialOperation)
+  const [operation, setOperation] = useState(initialOperation);
 
   const onChangeHandler = (event) => {
     const {
       total: newTotal,
       next: newNext,
       operation: newOperation,
-    } = calculate({ total, next, operation },  event.target.innerText);
+    } = calculate({ total, next, operation }, event.target.innerText);
     setTotal(newTotal);
     setNext(newNext);
     setOperation(newOperation);
   };
 
-    return (
+  return (
+    <>
+      <div>
+        <p>Let&#39;s do some math!</p>
+      </div>
       <div className="container w-75">
         <div className="row">
-          <div className="col bg-secondary text-end border-secondary text-white">{next||operation||total}</div>
+          <div className="col bg-secondary text-end border-secondary text-white">{next || operation || total}</div>
         </div>
-				<div className="row">
-          <button className="col-3 bg-light text-center border-secondary border border-1" onClick={onChangeHandler}>AC</button>
-					<button className="col-3 bg-light text-center border-secondary border border-1" onClick={onChangeHandler}>+/-</button>
-					<button className="col-3 bg-light text-center border-secondary border border-1" onClick={onChangeHandler}>%</button>
-					<button className="col-3 bg-warning text-center border-secondary border border-1" onClick={onChangeHandler}>÷</button>
+        <div className="row">
+          <button className="col-3 bg-light text-center border-secondary border border-1" onClick={onChangeHandler} type="button">AC</button>
+          <button className="col-3 bg-light text-center border-secondary border border-1" onClick={onChangeHandler} type="button">+/-</button>
+          <button className="col-3 bg-light text-center border-secondary border border-1" onClick={onChangeHandler} type="button">%</button>
+          <button className="col-3 bg-warning text-center border-secondary border border-1" onClick={onChangeHandler} type="button">÷</button>
         </div>
-				<div className="row">
-          <button className="col-3 bg-light text-center border-secondary border border-1" onClick={onChangeHandler}>7</button>
-					<button className="col-3 bg-light text-center border-secondary border border-1" onClick={onChangeHandler}>8</button>
-					<button className="col-3 bg-light text-center border-secondary border border-1" onClick={onChangeHandler}>9</button>
-					<button className="col-3 bg-warning text-center border-secondary border border-1" onClick={onChangeHandler}>x</button>
+        <div className="row">
+          <button className="col-3 bg-light text-center border-secondary border border-1" onClick={onChangeHandler} type="button">7</button>
+          <button className="col-3 bg-light text-center border-secondary border border-1" onClick={onChangeHandler} type="button">8</button>
+          <button className="col-3 bg-light text-center border-secondary border border-1" onClick={onChangeHandler} type="button">9</button>
+          <button className="col-3 bg-warning text-center border-secondary border border-1" onClick={onChangeHandler} type="button">x</button>
         </div>
-				<div className="row">
-          <button className="col-3 bg-light text-center border-secondary border border-1" onClick={onChangeHandler}>4</button>
-					<button className="col-3 bg-light text-center border-secondary border border-1" onClick={onChangeHandler}>5</button>
-					<button className="col-3 bg-light text-center border-secondary border border-1" onClick={onChangeHandler}>6</button>
-					<button className="col-3 bg-warning text-center border-secondary border border-1" onClick={onChangeHandler}>-</button>
+        <div className="row">
+          <button className="col-3 bg-light text-center border-secondary border border-1" onClick={onChangeHandler} type="button">4</button>
+          <button className="col-3 bg-light text-center border-secondary border border-1" onClick={onChangeHandler} type="button">5</button>
+          <button className="col-3 bg-light text-center border-secondary border border-1" onClick={onChangeHandler} type="button">6</button>
+          <button className="col-3 bg-warning text-center border-secondary border border-1" onClick={onChangeHandler} type="button">-</button>
         </div>
-				<div className="row">
-          <button className="col-3 bg-light text-center border-secondary border border-1" onClick={onChangeHandler}>1</button>
-					<button className="col-3 bg-light text-center border-secondary border border-1" onClick={onChangeHandler}>2</button>
-					<button className="col-3 bg-light text-center border-secondary border border-1" onClick={onChangeHandler}>3</button>
-					<button className="col-3 bg-warning text-center border-secondary border border-1" onClick={onChangeHandler}>+</button>
+        <div className="row">
+          <button className="col-3 bg-light text-center border-secondary border border-1" onClick={onChangeHandler} type="button">1</button>
+          <button className="col-3 bg-light text-center border-secondary border border-1" onClick={onChangeHandler} type="button">2</button>
+          <button className="col-3 bg-light text-center border-secondary border border-1" onClick={onChangeHandler} type="button">3</button>
+          <button className="col-3 bg-warning text-center border-secondary border border-1" onClick={onChangeHandler} type="button">+</button>
         </div>
-				<div className="row">
-          <button className="col-6 bg-light text-center border-secondary border border-1" onClick={onChangeHandler}>0</button>
-					<button className="col-3 bg-light text-center border-secondary border border-1" onClick={onChangeHandler}>.</button>
-					<button className="col-3 bg-warning text-center border-secondary border border-1" onClick={onChangeHandler}>=</button>
+        <div className="row">
+          <button className="col-6 bg-light text-center border-secondary border border-1" onClick={onChangeHandler} type="button">0</button>
+          <button className="col-3 bg-light text-center border-secondary border border-1" onClick={onChangeHandler} type="button">.</button>
+          <button className="col-3 bg-warning text-center border-secondary border border-1" onClick={onChangeHandler} type="button">=</button>
         </div>
       </div>
-    );
-}
-
+    </>
+  );
+};
 export default Calculator;
--- src/pages/quote.jsx
+++ src/pages/quote.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const Quote = () => {
   return (
     <div>
-      <h2>Quote</h2>
+      <h2>
+        Mathematics is not about numbers, equations, computations
+        <br>or algorithms: it is about understanding.- William Paul Thurston</br>
+      </h2>
     </div>
   );
 };
